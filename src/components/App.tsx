e5ef097993705422354e3d1238f9e2f089e7903e
Tony Kang <taekang1618@gmail.com>
2025-01-05 01:19:36 +0900

    basic personal website template with mobile support

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './header/Header';
+import Footer from './footer/Footer';
+import Home from './home/Home';
+import About from './about/About';
+import Contact from './contact/Contact';
 
 const App: React.FC = () => {
     return (
-        <div>
-            <h1>Welcome to Yaongi World!</h1>
-        </div>
+        <Router>
+            <Header />
+            <main>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/contact" element={<Contact />} />
+                </Routes>
+            </main>
+            <Footer />
+        </Router>
     );
 };
 
